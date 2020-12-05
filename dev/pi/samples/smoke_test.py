c5939081c21df7a50dc6ecea4ded2c00c2902b38
Sumit Kumar Maitra <code@sumitmaitra.com>
2020-12-05 21:30:03 +0000

    Code fixes

--- dev/pi/samples/smoke_test.py
+++ dev/pi/samples/smoke_test.py
@@ -5,23 +5,35 @@ import sys
 import time
 import serial
 import picamera
+from datetime import datetime
+import atexit
 
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(__file__), "../libs/smokey")))
 
 from smokey import Smokey
 
+
 miniMecanum = Smokey()
 frame = 0
-folderName = datetime.strptime(str(date), '%Y-%m-%d-%H:%M:%S')
+date = datetime.now()
+folderName = 'captures/' + date.strftime('%Y-%m-%d-%H-%M-%S')
+if not os.path.exists(folderName):
+    os.makedirs(folderName)
+
+def cleanup():
+    miniMecanum.set_speed(0)
+
+atexit.register(cleanup)
 
 with picamera.PiCamera() as camera:
     camera.resolution = (128, 128)
     camera.start_preview()
     time.sleep(2)
-    miniMecanum.set_speed(128)
+    miniMecanum.set_speed(-128)
     while True:
-        timestamp = datetime.strptime(str(date), '%H:%M:%S')
-        name = 'captures/' + folderName + '/' + 'image' + timestamp+ '-' + str(frame) + '.data'
+        date = datetime.now()
+        timestamp = date.strftime('%H-%M-%S')
+        name = folderName + '/' + 'image' + timestamp + '-' + str(frame) + '.data'
         frame = frame + 1
         camera.capture(name, 'yuv')
