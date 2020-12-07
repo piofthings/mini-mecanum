31f5709ea85629a78861d3cda377f2ec37766174
Sumit Kumar Maitra <code@sumitmaitra.com>
2020-12-07 02:18:06 +0000

    Smoke_Test not working

--- dev/pi/samples/smoke_test.py
+++ dev/pi/samples/smoke_test.py
@@ -5,6 +5,7 @@ import sys
 import time
 import serial
 import picamera
+import picamera.array
 from datetime import datetime
 import atexit
 import numpy as np
@@ -25,23 +26,23 @@ class SmokeyTest:
         atexit.register(self.cleanup)
 
     def garden_path(self):
-        date = datetime.now()
-
-
-        with picamera.PiCamera() as camera:
-            camera.resolution = (32, 32)
-            camera.start_preview()
-            time.sleep(2)
-            miniMecanum.set_speed(128)
-            date = datetime.now()
-            
-            print(date.strftime('%H-%M-%S-%f')[:-3])
-            while True:
-                __frame = __frame + 1
-                output = np.empty((32, 32, 3), dtype=np.uint8)
-                camera.capture(output, 'yuv')
-                np.vstack((self.__mem_buffer,output))
-
+     date = datetime.now()
+     with picamera.PiCamera() as camera:
+      camera.resolution = (32, 32)
+      camera.framerate = 24
+      camera.start_preview()
+      time.sleep(2)
+#      self.__miniMecanum.set_speed(128)
+      while True:
+       with picamera.array.PiYUVArray(camera) as output:
+        camera.capture(output, 'yuv')
+        print('Captured %dx%d image' % (
+              output.array.shape, output.array.shape[0]))
+#        date = datetime.now()
+#        print(date.strftime('%H-%M-%S-%f')[:-3])
+#        np.hstack((self.__mem_buffer,output.array))
+#        self.__frame = self.__frame + 1
+#        print(self.__frame)
 
     def cleanup(self):
         try:
@@ -49,7 +50,7 @@ class SmokeyTest:
             print(self.__frame)
             print(date.strftime('%H-%M-%S-%f')[:-3])
 
-            fileName = 'captures/' + date.strftime('%Y-%m-%d-%H-%M-%S')
+            folderName = 'captures/' + date.strftime('%Y-%m-%d-%H-%M-%S')
             if not os.path.exists(folderName):
                 os.makedirs(folderName)
             np.save(folderName + "/" + date.strftime('%H-%M-%S-%f')[:-3], self.__mem_buffer)
